7c036df53c55faabdd4ea76d1f6fa2ab96027c9b
Henrique Schmidt <henrique.rieger@gmail.com>
2024-02-14 16:49:37 -0300

    test

--- src/test/java/br/dev/hc/TestAppium/utils/Hooks.java
+++ src/test/java/br/dev/hc/TestAppium/utils/Hooks.java
@@ -2,24 +2,55 @@ package br.dev.hc.TestAppium.utils;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.InetAddress;
 
 public class Hooks {
   private static AndroidDriver driver;
+  private static AppiumDriverLocalService service;
+
+  private static String getHostname() {
+    String hostname = "Unknown";
+
+    try {
+      InetAddress addr;
+      addr = InetAddress.getLocalHost();
+      hostname = addr.getHostName();
+    }
+    catch (Exception ex) {
+      System.out.println("Hostname can not be resolved");
+    }
+
+    return hostname;
+  }
 
   @Before
-  public static void startDriver() throws MalformedURLException {
+  public static void startDriver() {
+    service = AppiumDriverLocalService.buildDefaultService();
+    service.start();
+
+    final String id = "appium-" + getHostname();
+    final String apk = System.getProperty("user.dir") + "/apks/app-debug.apk";
+
     String ci = System.getenv("CI");
     UiAutomator2Options options = new UiAutomator2Options()
-      .setUdid("aaa")
-      .setApp("/home/myapp.apk");
+      .setUdid(id)
+      .setApp(apk);
 
-    driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+    /*
+    XCUITestOptions options = new XCUITestOptions()
+      .setUdid('123456')
+      .setApp("/home/myapp.ipa");
+    IOSDriver driver = new IOSDriver(
+      // The default URL in Appium 1 is http://127.0.0.1:4723/wd/hub
+      new URL("http://127.0.0.1:4723"), options
+    );
+    */
+
+    driver = new AndroidDriver(service.getUrl(), options);
 
 
     if (ci != null && ci.equals("true")) {
@@ -30,11 +61,16 @@ public class Hooks {
 
   @After
   public static void closeDriver(){
-    driver.close();
+    if (driver != null) {
+      driver.quit();
+    }
+
+    if (service != null) {
+      service.stop();
+    }
   }
 
-  public static WebDriver getDriver(){
+  public static AndroidDriver getDriver(){
     return driver;
   }
-
 }
